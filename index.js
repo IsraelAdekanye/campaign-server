01400023e08aa50b520aa5ba01d602c65a401521
IsraelMacBook <israeladedigba@yahoo.com>
2022-11-21 13:28:46 +0100

    fixing response to client

--- index.js
+++ index.js
@@ -17,22 +17,6 @@ app.use(cors({
 
 app.use('/transaction', cors(), transaction);
 
-// const config = {
-//   method: 'get',
-//   url: 'https://api.paystack.co/transaction',
-//   headers: { 
-//     'Accept': 'application/json', 
-//     'Authorization': `Bearer ${process.env.SECRET_KEY}`
-//   }
-// };
-
-// axios(config)
-// .then(function (response) {
-//   console.log(JSON.stringify(response.data));
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
 
 app.listen(process.env.PORT, ()=>{
     console.log(`You have Started Server on PORT ${process.env.PORT}`);
